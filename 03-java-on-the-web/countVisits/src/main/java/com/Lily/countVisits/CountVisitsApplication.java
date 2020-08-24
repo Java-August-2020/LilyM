64a6a6a7f70aaf9b6c1114785032a6ed62ab7c70
lili-mar <lilimar@aol.com>
2020-08-23 19:35:13 -0500

    initial commit

---
+++ 03-java-on-the-web/countVisits/src/main/java/com/Lily/countVisits/CountVisitsApplication.java
@@ -0,0 +1,13 @@
+package com.Lily.countVisits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountVisitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountVisitsApplication.class, args);
+	}
+
+}
